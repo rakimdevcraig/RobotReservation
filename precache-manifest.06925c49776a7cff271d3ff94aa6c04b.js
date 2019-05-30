3648b7ed54baee14b507e70f6c59b4f6ec741deb
Rakim <rakimdevcraig>
2019-05-30 10:35:35 -0400

    Updates

---
+++ precache-manifest.06925c49776a7cff271d3ff94aa6c04b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bf1adca0298c1e356f6",
+    "url": "/RobotReservation/static/js/runtime~main.7bf1adca.js"
+  },
+  {
+    "revision": "be4482a9802523db22e7",
+    "url": "/RobotReservation/static/js/main.be4482a9.chunk.js"
+  },
+  {
+    "revision": "a4e01a59f4a53afef40c",
+    "url": "/RobotReservation/static/js/1.a4e01a59.chunk.js"
+  },
+  {
+    "revision": "be4482a9802523db22e7",
+    "url": "/RobotReservation/static/css/main.6d4e2d01.chunk.css"
+  },
+  {
+    "revision": "6c96969366119268635016fa3e346b21",
+    "url": "/RobotReservation/index.html"
+  }
+];
